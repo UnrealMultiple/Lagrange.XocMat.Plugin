e4292848d9a6fe872199f0b49baf95cc561ef869
Controllerdestiny <523321293@qq.com>
2025-02-18 01:19:46 +0800

    update

---
+++ src/CommandUtils/Config.cs
@@ -0,0 +1,62 @@
+﻿using Lagrange.XocMat.Configuration;
+using Newtonsoft.Json;
+using Lagrange.XocMat.Attributes;
+
+namespace CommandUtils;
+
+[ConfigSeries]
+public class Config : JsonConfigBase<Config>
+{
+    [JsonProperty("启用")]
+    public bool Enabled { get; set; } = true;
+
+    [JsonProperty("群禁指令")]
+    public Dictionary<long, CommandBody> GroupDisabledCommands { get; set; } = [];
+
+    public bool AddDisabledCommand(long groupId, string command)
+    {
+        if (!GroupDisabledCommands.TryGetValue(groupId, out CommandBody? value))
+        {
+            value = new CommandBody();
+            GroupDisabledCommands[groupId] = value;
+        }
+        return value.DisabledCommands.Add(command);
+    }
+
+    public bool RemoveDisabledCommand(long groupId, string command)
+    {
+        if (GroupDisabledCommands.TryGetValue(groupId, out CommandBody? value))
+        {
+            return value.DisabledCommands.Remove(command);
+        }
+        return false;
+    }
+
+    public bool AddAllowedCommand(long groupId, string command)
+    {
+        if (!GroupDisabledCommands.TryGetValue(groupId, out CommandBody? value))
+        {
+            value = new CommandBody();
+            GroupDisabledCommands[groupId] = value;
+        }
+        return value.AllowedCommands.Add(command);
+    }
+
+    public bool RemoveAllowedCommand(long groupId, string command)
+    {
+        if (GroupDisabledCommands.TryGetValue(groupId, out CommandBody? value))
+        {
+            return value.AllowedCommands.Remove(command);
+        }
+        return false;
+    }
+}
+
+public class CommandBody
+{
+    [JsonProperty("禁用指令")]
+    public HashSet<string> DisabledCommands { get; set; } = [];
+
+    [JsonProperty("允许指令")]
+    public HashSet<string> AllowedCommands { get; set; } = [];
+}
---
+++ src/CommandUtils/Plugin.cs
@@ -0,0 +1,41 @@
+﻿using Lagrange.Core;
+using Lagrange.XocMat.Commands;
+using Lagrange.XocMat.Extensions;
+using Lagrange.XocMat.Plugin;
+using Lagrange.XocMat.Attributes;
+using Microsoft.Extensions.Logging;
+
+namespace CommandUtils;
+
+public class Plugin(ILogger logger, CommandManager commandManager, BotContext bot) : XocMatPlugin(logger, commandManager, bot)
+{
+    public override void Initialize()
+    {
+        Lagrange.XocMat.Event.OperatHandler.OnCommand += OnCommand;
+        //CommandManager.AddGroupCommand(new("cmd", DisableCommand, "onebot.cmd.utils"));
+    }
+
+    private async ValueTask OnCommand(CommandArgs args)
+    {
+        if(Config.Instance.GroupDisabledCommands.TryGetValue(args.EventArgs.Chain.GroupUin!.Value, out CommandBody? commandBody))
+        {
+            if (commandBody.DisabledCommands.Count > 0 && commandBody.DisabledCommands.Contains(args.Name))
+            {
+                await args.EventArgs.Reply("此指令已被禁用");
+                args.Handler = true;
+                return;
+            }
+            if (commandBody.AllowedCommands.Count > 0 && !commandBody.AllowedCommands.Contains(args.Name))
+            {
+                await args.EventArgs.Reply("此指令未被允许");
+                args.Handler = true;
+            }
+        }
+    }
+
+
+    protected override void Dispose(bool dispose)
+    {
+        Lagrange.XocMat.Event.OperatHandler.OnCommand -= OnCommand;
+    }
+}
