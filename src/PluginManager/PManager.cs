1ddaee845de12a05d983e076eae359b979502770
Controllerdestiny <523321293@qq.com>
2025-02-22 05:24:55 +0800

    UODATE

--- src/PluginManager/PManager.cs
+++ src/PluginManager/PManager.cs
@@ -4,20 +4,11 @@ using Lagrange.XocMat;
 using Lagrange.XocMat.Command;
 using Lagrange.XocMat.Command.CommandArgs;
 using Lagrange.XocMat.Extensions;
-using Lagrange.XocMat.Plugin;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace PluginManager;
 
 public class PManager : Command
 {
-    private readonly PluginLoader _loader;
-
-    public PManager()
-    {
-        _loader = XocMatApp.Instance.Services.GetRequiredService<PluginLoader>();
-    }
-
     public override string HelpText => "插件管理";
     public override string[] Alias => ["pm"];
     public override string[] Permissions => ["onebot.plugin.admin"];
@@ -36,7 +27,7 @@ public class PManager : Command
             sb.AppendLine("|序号|插件名称|插件作者|插件说明|插件版本|启用|");
             sb.AppendLine("|:--:|:--:|:--:|:--:|:--:|:--:|");
             int index = 1;
-            foreach (var plugin in _loader.PluginContext.Plugins)
+            foreach (var plugin in XocMatAPI.PluginLoader.PluginContext.Plugins)
             {
                 sb.AppendLine($"|{index}|{plugin.Plugin.Name}|{plugin.Plugin.Author}|{plugin.Plugin.Description}|{plugin.Plugin.Version}|{plugin.Initialized}|");
                 index++;
@@ -48,12 +39,12 @@ public class PManager : Command
         }
         else if (args.Parameters.Count == 2 && args.Parameters[0].ToLower() == "off")
         {
-            if (!int.TryParse(args.Parameters[1], out var index) || index < 1 || index > _loader.PluginContext.Plugins.Count)
+            if (!int.TryParse(args.Parameters[1], out var index) || index < 1 || index > XocMatAPI.PluginLoader.PluginContext.Plugins.Count)
             {
                 await args.Event.Reply("请输入一个正确的序号!", true);
                 return;
             }
-            var instance = _loader.PluginContext.Plugins[index - 1];
+            var instance = XocMatAPI.PluginLoader.PluginContext.Plugins[index - 1];
             if (!instance.Initialized)
             {
                 await args.Event.Reply("此插件已经被卸载，无需重复卸载!!", true);
@@ -64,12 +55,12 @@ public class PManager : Command
         }
         else if (args.Parameters.Count == 2 && args.Parameters[0].ToLower() == "on")
         {
-            if (!int.TryParse(args.Parameters[1], out var index) || index < 1 || index > _loader.PluginContext.Plugins.Count)
+            if (!int.TryParse(args.Parameters[1], out var index) || index < 1 || index > XocMatAPI.PluginLoader.PluginContext.Plugins.Count)
             {
                 await args.Event.Reply("请输入一个正确的序号!", true);
                 return;
             }
-            var instance = _loader.PluginContext.Plugins[index - 1];
+            var instance = XocMatAPI.PluginLoader.PluginContext.Plugins[index - 1];
             if (instance.Initialized)
             {
                 await args.Event.Reply("此插件已经被启用，无需重复启用!!", true);
@@ -80,8 +71,8 @@ public class PManager : Command
         }
         else if (args.Parameters.Count == 1 && args.Parameters[0].ToLower() == "reload")
         {
-            _loader.UnLoad();
-            _loader.Load();
+            XocMatAPI.PluginLoader.UnLoad();
+            XocMatAPI.PluginLoader.Load();
             await args.Event.Reply("插件列表已经重新加载!", true);
         }
         else
